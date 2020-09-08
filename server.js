d60f2597fd930060508e67713c136a1866677307
LAPTOP-6UTP5SVL\PRAMOD <PramodChinni87@gmail.com>
2020-09-08 21:22:03 +0530

    corrected server js

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ var path = require("path");
 var app = express();
 
 app.use(express.static(path.join(__dirname, "/dist/launch-programs")));
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/dist/launch-programs/index.html"));
-  //res.sendFile(path.join(__dirname + "/dist/launch-programs/browser/index.html"));
-});
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname + "/dist/launch-programs/index.html"));  
+// });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running...");
